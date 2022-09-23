60d70d8f4856eb258bfc2ca27de64e88fc73cfe9
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-09-23 09:02:14 -0700

    changes

---
+++ functions/property.js
@@ -0,0 +1,31 @@
+// http://localhost:8888/api/property
+require('dotenv').config()
+const Airtable = require('airtable-node')
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base(process.env.AIRTABLE_BASE)
+  .table('realestate')
+
+  exports.handler = async(event, context) => {
+    try {
+        const {records} = await airtable.list()
+        const properties = records.map((property) => {
+            const {id} = property 
+            const {images, sqft, bed, bath, price, city, state} = property.fields
+            const url = images[0].url 
+            return {sqft, bed, bath, price, url, id, city, state}
+        })
+        return {
+            headers: {
+                'Access-Control-Allow-Origin' : '*'
+            },
+            statusCode: 200,
+            body: JSON.stringify(properties)
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'There was a server error'
+        }
+    }
+}
---
+++ functions/single-property.js
@@ -0,0 +1,65 @@
+// http://localhost:8888/api/single-property?id=
+require('dotenv').config()
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base(process.env.AIRTABLE_BASE)
+  .table('realestate')
+
+  exports.handler = async (event, context) => {
+    const {id} = event.queryStringParameters 
+
+    // FLATTEN THE OBJECT
+    const flatten = (ob) => {
+        let result = {};
+        for (const i in ob) {
+            if ((typeof ob[i]) === 'object' && !Array.isArray(ob[i])) {
+                const temp = flatten(ob[i]);
+                for (const j in temp) {
+                    result[j] = temp[j];
+                }
+            }
+            else {
+                result[i] = ob[i];
+            }
+        }
+        return result;
+    };
+
+    if(id){
+        try {
+            const property = await airtable.retrieve(id)
+            if(property.error){
+                return {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    statusCode: 404,
+                    body: `No product with id: ${id}`
+                }
+            }
+            return {
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                },
+                statusCode: 200,
+                body: JSON.stringify(flatten(property))
+            }
+        } catch (error) {
+            return {
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                },
+                statusCode: 500,
+                body:  `Server Error`
+            }
+        }
+    }
+    return {
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        statusCode: 400,
+        body:  `Please provide product id`
+    }
+  }
